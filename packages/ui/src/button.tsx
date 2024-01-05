13fef1af533caca1925bc3c3bf4cb6552b4902c4
Alex Padilla <0xpadimaster@gmail.com>
2024-01-05 15:20:33 -0500

    set up tailwindcss

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -5,14 +5,14 @@ import { ReactNode } from "react";
 interface ButtonProps {
   children: ReactNode;
   className?: string;
-  appName: string;
+  onClick?: () => void;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className, onClick }: ButtonProps) => {
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      className={`bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-full cursor-default ${className}`}
+      onClick={onClick}
     >
       {children}
     </button>
